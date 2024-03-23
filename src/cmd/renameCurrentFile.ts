4010993e6f1f97fe44710c04180269316d4387cb
dlguswo333 <dlguswo002@gmail.com>
2024-03-23 21:02:17 +0900

    Implement rename the current file command

---
+++ src/cmd/renameCurrentFile.ts
@@ -0,0 +1,45 @@
+import * as vscode from 'vscode';
+
+const command = vscode.commands.registerCommand('vscode-more-file-command.renameCurrentFile', () => {
+  const currentFile = vscode.window.activeTextEditor?.document;
+  if (!currentFile) {
+    return;
+  }
+  const currentFileName = currentFile.fileName.split('/').at(-1);
+  if (!currentFileName) {
+    return;
+  }
+
+  const inputRes = vscode.window.showInputBox({
+    title: 'Insert the new file for the current file:',
+    value: currentFileName,
+    validateInput: (value) => {
+      if (value.includes('/') || value.includes('\\')) {
+        return 'Renaming the file name including path separator is not supported.';
+      }
+      return undefined;
+    },
+  });
+
+  inputRes.then(async (value) => {
+    if (!value || !(value = value.trim())) {
+      return;
+    }
+    const targetUri = vscode.Uri.joinPath(currentFile.uri, '../', value);
+    try {
+      // stat will fail if the target uri does not exist.
+      await vscode.workspace.fs.stat(targetUri);
+      vscode.window.showErrorMessage(`The file name '${value}' already exists.`);
+      return;
+    } catch {
+      try {
+        await vscode.workspace.fs.rename(currentFile.uri, targetUri, {overwrite: false});
+        vscode.window.showInformationMessage(`Renamed the current file name to '${value}'.`);
+      } catch {
+        vscode.window.showErrorMessage(`Renaming the current file as '${value}' failed for unknown reasons.`);
+      }
+    }
+  });
+});
+
+export default command;
