950f6c885ac37d16444bbb32b747cdad8751e322
dlguswo333 <dlguswo002@gmail.com>
2024-03-23 18:29:07 +0900

    Setup eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  root: true,
+  ignorePatterns: ['out', 'dist', '**/*.d.ts', '.eslintrc.cjs'],
+  rules: {
+    'indent': ['error', 2, {SwitchCase: 2}],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'space-infix-ops': 'error',
+    'keyword-spacing': 'error',
+    'eol-last': 'error',
+    'comma-dangle': ['error', {
+      'arrays': 'never',
+      'objects': 'always-multiline',
+      'imports': 'never',
+      'exports': 'never',
+      'functions': 'never'
+    }],
+    'object-curly-spacing': 'error',
+    'space-before-function-paren': ['error', 'always'],
+    'space-before-blocks': ['error', 'always'],
+    'arrow-spacing': 'error',
+    '@typescript-eslint/type-annotation-spacing': ['error', {
+      before: false,
+      after: true,
+      overrides: {
+        arrow: {before: true, after: true}
+      }
+    }]
+  }
+};
--- src/extension.ts
+++ src/extension.ts
@@ -4,23 +4,23 @@ import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate (context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-more-file-command" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "vscode-more-file-command" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-more-file-command.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-more-file-command!');
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand('vscode-more-file-command.helloWorld', () => {
+    // The code you place here will be executed every time your command is executed
+    // Display a message box to the user
+    vscode.window.showInformationMessage('Hello World from vscode-more-file-command!');
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate () { }
