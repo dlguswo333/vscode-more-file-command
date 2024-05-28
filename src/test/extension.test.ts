313188944da4d64376ecfffa873a83530fccea2d
dlguswo333 <dlguswo002@gmail.com>
2024-05-28 23:07:13 +0900

    [#11] Implement getRelativeUriPath util function

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,5 +1,5 @@
 import * as assert from 'assert';
-import {getFileNameFromPath} from '@/util';
+import {getFileNameFromPath, getRelativeUriPath} from '@/util';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -9,8 +9,27 @@ import * as vscode from 'vscode';
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.');
 
-  test('Util test', () => {
+  test('Util getFileNameFromPath:', () => {
     assert.strictEqual(getFileNameFromPath('path/to/file.txt'), 'file.txt');
     assert.strictEqual(getFileNameFromPath('path\\to\\file.txt'), 'file.txt');
+  });
+
+  test('Util getRelativeUriPath:', () => {
+    const Uri = vscode.Uri;
+
+    assert.strictEqual(
+      getRelativeUriPath(
+        Uri.from({scheme: 'file', path: '/foo/bar/one/two'}),
+        Uri.from({scheme: 'file', path: '/foo/bar'})
+      ),
+      'bar/one/two'
+    );
+    assert.strictEqual(
+      getRelativeUriPath(
+        Uri.from({scheme: 'file', path: '/foo/bar'}),
+        Uri.from({scheme: 'file', path: '/foo/bar'})
+      ),
+      'bar'
+    );
   });
 });
